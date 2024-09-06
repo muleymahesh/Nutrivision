11b742ebc6a7c6bbea47858198a755f4135b3131
muleymahesh <muley.mahesh@gmail.com>
2024-09-06 12:46:50 +0530

    Initial commit

---
+++ app/src/main/java/com/maks/nutrivision/common/NavigationStack.kt
@@ -0,0 +1,29 @@
+package com.maks.nutrivision.common
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+
+@Composable
+fun NavigationStack() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Screen.Main.route) {
+        composable(route = Screen.Main.route) {
+            MainScreen(navController = navController)
+        }
+        composable(
+            route = Screen.Detail.route + "?text={text}",
+            arguments = listOf(
+                navArgument("text") {
+                    type = NavType.StringType
+                    nullable = true
+                }
+            )
+        ) {
+            DetailScreen(text = it.arguments?.getString("text"))
+        }
+    }
+}
